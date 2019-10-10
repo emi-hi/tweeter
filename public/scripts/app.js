cbd695c57ca27f79710a12f1840f91ce0d194b86
emi-hi <emily.hillier@gmail.com>
2019-10-10 02:08:11 +0000

    added function for checking validation, refactored post function to submit entire form

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,31 +1,41 @@
 $(document).ready(() => {
+
+  const escape =  function(str) {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  }
+
+
   const createTweetElement = function(tweetObj) {
+
+    // const safeHTML = `<p>${escape(tweetObj.content.text)}</p>`;
     const $tweet = $("<article>").addClass("tweet");
     const d = new Date(tweetObj.created_at);
-    const today = new Date();
-    const todayTime = today.getTime()
-    ms = Math.abs(todayTime - d)
-    let days = (ms / (60*60*24*1000))
-    let timeSinceTweet = '';
-    //dates logic..
-    if (days < 30) {
-      timeSinceTweet += days + ' days ago';
-    } else if (days > 30 && days <= 45) {
-      timeSinceTweet = '1 month ago';
-    } else if (days > 45 && days <= 365) {
-      timeSinceTweet = Math.round(days/12) + ' months ago';
-    } else {
-      timeSinceTweet = Math.round(days/365) + ' years ago';
-    }
+    // const today = new Date();
+    // const todayTime = today.getTime()
+    // ms = Math.abs(todayTime - d)
+    // let days = (ms / (60*60*24*1000))
+    // let timeSinceTweet = '';
+    // //dates logic..
+    // if (days < 30) {
+    //   timeSinceTweet += days + ' days ago';
+    // } else if (days > 30 && days <= 45) {
+    //   timeSinceTweet = '1 month ago';
+    // } else if (days > 45 && days <= 365) {
+    //   timeSinceTweet = Math.round(days/12) + ' months ago';
+    // } else {
+    //   timeSinceTweet = Math.round(days/365) + ' years ago';
+    // }
     const markup = `
         <header>
           <img src=${tweetObj.user.avatars}> 
           <p>${tweetObj.user.name}</p>
           <h4>${tweetObj.user.handle}</h4>
         </header>
-        <p>${tweetObj.content.text}</p>
+        <p>${escape(tweetObj.content.text)}</p>
         <footer>
-          <p> ${timeSinceTweet}
+          <p> ${d}
               <a>🚩🔁❤️</a>
           </p>
         </footer>
@@ -35,34 +45,50 @@ $(document).ready(() => {
 
   const renderTweets = function(tweetArray) {
     for (let tweet of tweetArray) {
-      $('#tweets-container').append(createTweetElement(tweet));
+      $('#tweets-container').prepend(createTweetElement(tweet));
     }
-    
   }
 
+  // get the tweets from the server and render
   const loadTweets = function() {
     $.ajax({url: '/tweets', type: 'GET'})
       .then(function(moreTweets) {
         renderTweets(moreTweets)
       })
   }
-  loadTweets()
-
-  const button = document.getElementById('submitTweet');
-  $(button).on('click', function () {
-    event.preventDefault()
+  const isTweetValid = function(theInput) {
     const tweetLimit = 140;
-    const $inputSerialize = $( this.form ).serialize()
-    const theInput = $('textArea#tweetInput').val()
-    if( theInput === '' | theInput === null){
-      alert("Please enter a tweet!")
+     //if the tweet is too short or long, reject and send an alert to the user
+    if ( theInput === '' | theInput === null){
+      return("Please enter a tweet!")
     } else if (theInput.length > tweetLimit) {
-      alert("please shorten your tweet, the max is 140 characters!")
+      return("please shorten your tweet, the max is 140 characters!")
     } else {
+      return true
+    }
+  }
+
+  // when the button is clicked, check the text input and if it's good, send it to the server! then reset the form and load tweets.
+  $(".form-inline").on('submit', function () {
+    event.preventDefault()
+    const $form = $(this);
+    const $input = $form.find("textarea");
+    // const theInput = $input.val(); //switch this to this!
+    const $inputSerialize = $form.serialize();
+    if (isTweetValid($input.val()) === true) {
       $.ajax({url: '/tweets', type: 'POST', data: $inputSerialize})
-      .then($(this.form).trigger("reset"));
+        .then((res) => {
+          $form.trigger("reset");
+          $(this).siblings("span.counter").text(140);
+          (loadTweets())
+        })
+    } else {
+      alert(isTweetValid($input.val()));
     }
   });
+
+  loadTweets()
+  
 
 });
 
